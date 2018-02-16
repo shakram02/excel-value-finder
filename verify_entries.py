6cd507337e9c2f716c764cb88ab6967b7e0234ba
shakram02 <ahmedhamdyau@gmail.com>
2018-02-16 07:46:52 +0200

    abstract to a filter column function

--- verify_entries.py
+++ verify_entries.py
@@ -15,41 +15,68 @@ optionParser.add_argument("--column", help="Column name contianing needle values
 
 # optionParser.add_argument("haystack", help="Bigger file to search for items")
 
+def filter_column(ws_column, is_accepted, is_unique=None, on_duplicate=None):
+    """
+    Pass on all form entries, add cell values if the cell meets acceptance criteria
+
+    Checking for duplicates won't work without providing both ``is_unique`` and ``on_duplicate``
+
+
+    :param ws_column: Column of the worksheet (idc about field header, caller removes it)
+    :param is_accepted: Function pointer to test acceptance
+    :param is_unique: Function pointer to check uniqueness
+    :param on_duplicate: Callback function on finding duplicates
+    :return:
+    """
+    accepted = []
+    rejected = []
+
+    for item in ws_column:
+        cell: Cell = item
+
+        if cell.value is None:
+            continue
+
+        value = cell.value.strip()
+
+        if is_accepted(cell):
+            accepted.append(value)
+            if is_unique is not None and on_duplicate is not None and not is_unique(cell):
+                on_duplicate(cell)
+        else:
+            rejected.append(value)
+
+    return accepted, rejected
+
+
 def extract_valid_applicants(wb_path, sheet_name, column, reject_colors):
     wb = load_workbook(filename=wb_path)
 
     form_responses: Worksheet = wb[sheet_name]
     name_set = set()
-    names = []
-    # Don't forget to add FF at the begning of the color
-    out_of_scope = 0
 
-    # Skip the first row, it's a column def.
-    for item in form_responses[column][1:]:
-        cell: Cell = item
-        if cell.value is None:
-            continue
+    # TODO, handle duplicates, create a triple of name, phone, phone or name, mail, phone or whateva
+    def on_dup(c):
+        print("Duplicate:", c.value)
 
-        if cell.fill.bgColor.rgb in reject_colors:
-            out_of_scope += 1
-        else:
-            # People in this set should've been called, lets see
-            applicant_name = cell.value.strip()
+    def is_accepted(c):
+        return c.fill.bgColor.rgb not in reject_colors
 
-            # TODO, handle duplicates, create a triple of name, phone, phone or name, mail, phone or whateva
-            if applicant_name in name_set:
-                print("Duplicate:", applicant_name)
+    def is_unique(c):
+        if c.value in name_set:
+            return False
 
-            name_set.add(applicant_name)
-            names.append(applicant_name)
+        name_set.add(c.value)
+        return True
 
-    print("{}/{} didn't pass application phase".format(out_of_scope, form_responses.max_row))
+    names, rejected = filter_column(form_responses[column][1:], is_accepted, is_unique, on_dup)
+    print("{}/{} didn't pass application phase".format(len(names), form_responses.max_row))
 
     from pprint import pprint
     pprint(name_set)
     print("\n\n\n\n\n\n\n\n")
     pprint(names)
-    print("Unique:", len(name_set), "All:", len(names))
+    print("Unique:", len(name_set), "Unique:", len(names))
     return names
 
 
@@ -62,6 +89,7 @@ def main():
 
     args = optionParser.parse_args()
 
+    # Don't forget to add FF (alpha channel) at the begning of the color
     reject_colors = ["FF434343", "FF999999"]
     sheet_name = 'Form Responses 1'
     applicants = extract_valid_applicants(args.needle, sheet_name, args.column, reject_colors)
